e448ab6ee9997ddf718ba37294cca25ed4b0c91a
Allen <glux7381@gmail.com>
2019-03-18 17:33:13 +0800

    Init Controller

--- app.js
+++ app.js
@@ -1,25 +1,48 @@
 
+// Budget Controller
 var budgetController = (function() {
 
-    var x = 23;
-
-    var add = function (a) {
-        return x + a;
-    }
-
-    return {
-        publicTest : function (b) {
-            console.log(add(b));
-        }
-    }
+    // Some Code
 
 }) ();
 
 
 
+// UI Controller
+var UIController = (function (){
+
+    // Some Code
+
+}) ();
 
 
 
+// Global APP Controller
+var  controller = (function(budgeCtrl, UICtrl) {
+
+    document.querySelector('.add__btn').addEventListener('click', function(){
+        
+        // 1. Get the field input data
+
+
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+
+    });
+
+    document.addEventListener('keypress', function (event){
+
+        
+
+    });
+
+}) (budgetController, UIController);
 
 
 
